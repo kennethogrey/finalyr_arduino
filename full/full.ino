ddfc4cd1aba0ab1dede70b22f199c9b7b441626a
kennethogrey <ogirekenneth@gmail.com>
2023-05-29 15:25:43 +0300

    updated full program

--- full/full.ino
+++ full/full.ino
@@ -1,93 +1,155 @@
-#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <RTClib.h>
 #include <Wire.h>
-
-RTC_DS3231 rtc;
-char t[32];
-
-static const int RXPin = 4, TXPin = 3;
-static const uint32_t GPSBaud = 9600;
+#include <string.h>
+#include <TinyGPS++.h>
 
 // The TinyGPS++ object
 TinyGPSPlus gps;
-
 // The serial connection to the GPS device
-SoftwareSerial gpsSerial(RXPin, TXPin);
-
-SoftwareSerial gsmSerial(5, 6);  // RX, TX
-
-int buttonState;
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 100;
+SoftwareSerial ss(4, 3);
+RTC_DS3231 rtc;
+char time[32];
+SoftwareSerial gprsSerial(5, 6);
+int buttonState = 0;
 
 void setup() {
   pinMode(2, INPUT);
-  buttonState = digitalRead(2);
-  Serial.begin(9600);
-  gpsSerial.begin(GPSBaud);
-  gsmSerial.begin(9600);  // Setting the baud rate of GSM Module
+  attachInterrupt(0, buttonPressed, CHANGE);
+  ss.begin(9600);
   Wire.begin();
   rtc.begin();
-  rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
-  //rtc.adjust(DateTime(2019, 1, 21, 5, 0, 0));
-  attachInterrupt(0, buttonPressed, CHANGE);
+  if (rtc.lostPower()) {
+    Serial.println("RTC is not running! Setting the time...");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+  gprsSerial.begin(9600);               // the GPRS baud rate
+  Serial.begin(9600);    // the GPRS baud rate
+  delay(1000);
 }
 
 void loop() {
   DateTime now = rtc.now();
-  sprintf(t, "%02d:%02d %02d/%02d/%02d", now.hour(), now.minute(), now.day(), now.month(), now.year());
-  
-  while (gpsSerial.available() > 0) {
-    gps.encode(gpsSerial.read());
-    if (gps.location.isUpdated()){
-      Serial.print("Latitude= ");
-      Serial.print(gps.location.lat(), 8);
-      Serial.print(" Longitude= ");
-      Serial.println(gps.location.lng(), 8);
+  sprintf(time, "%02d:%02d:%02d %02d-%02d-%02d", now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
+  Serial.print(F("Date/Time: "));
+  Serial.println(time);
+  delay(1000);
+
+  if (ss.available()) {
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()) {
+      float longitude = gps.location.lng();
+      float latitude = gps.location.lat();
+      Serial.print("Latitude = ");
+      Serial.print(latitude, 8);
+      Serial.print(" Longitude = ");
+      Serial.println(longitude, 8);
     }
   }
-  
-  unsigned long currentMillis = millis();
-  if (currentMillis - lastDebounceTime > debounceDelay) {
-    lastDebounceTime = currentMillis;
-    int newState = digitalRead(2);
-    if(buttonState != newState){
-      Serial.println("Button has been pressed");
-      buttonState = newState;
-      
-      Serial.println("Attach to GPRS");
-      gsmSerial.println("AT+CGATT=1\r");
-      delay(2000);
-      Serial.println("Setting the APN");
-      gsmSerial.println("AT+CSTT=\"airtelgprs.com\"\r");
-      delay(2000);
-      Serial.println("Bringing up the wireless connection");
-      gsmSerial.println("AT+CIICR\r");
-      delay(8000);
-      Serial.println("Getting local IP address");
-      gsmSerial.println("AT+CPIN?");
-      delay(5000);
-      while (gsmSerial.available() > 0) {
-        Serial.write(gsmSerial.read());
-      }
-      String lat = String(gps.location.lat(), 8);
-      String lng = String(gps.location.lng(), 8);
-      String time = String(t);
-      //Serial.println("Sending data to website: " + lat " " + lng + " " + time);
-      Serial.println("Sending data to website: " + lat + " " + lng + " " + time);
-      //gsmSerial.println("AT+HTTPPARA=\"URL\",\"https://gpsbse23-10.000webhostapp.com/gpsdata.php?lat="+lat+"&lon="+lng+"&time="+time+"\r"");
-      gsmSerial.println("AT+HTTPPARA=\"URL\",\"https://gpsbse23-10.000webhostapp.com/gpsdata.php?lat="+lat+"&lng="+lng+"&time="+time+"\"\r");
-      delay(2000);
-      gsmSerial.println("AT+HTTPACTION=0");
-      delay(20000);
-      while (gsmSerial.available() > 0) {
-        Serial.write(gsmSerial.read());
-      }
-    }
+
+  if (gprsSerial.available()) {
+    Serial.write(gprsSerial.read());
+    executeCommands();
+    sendData();
+    closeConnection();
   }
 }
 
-void buttonPressed(){
-  // do nothing, the code is handled in the main loop
+void executeCommands() {
+  sendCommand("AT",1000);
+  sendCommand("AT+CPIN?",1000);
+  sendCommand("AT+CREG?",1000);
+  sendCommand("AT+CGATT?",1000);
+  sendCommand("AT+CIPSHUT",1000);
+  sendCommand("AT+CIPSTATUS",2000);
+  sendCommand("AT+CIPMUX=0",2000);
+  sendCommand("AT+CSTT=\"internet\"",1000);
+  sendCommand("AT+CIICR",3000);
+  sendCommand("AT+CIFSR",2000);
+  sendCommand("AT+CIPSPRT=0",3000);
+  sendCommand("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"",6000);
+  sendCommand("AT+CIPSEND",4000);
+}
+
+void sendCommand(const char* command,int delayTime) {
+  gprsSerial.println(command);
+  delay(delayTime);
+  ShowSerialData();
+}
+
+void sendData() {
+  String str = "GET https://api.thingspeak.com/update?api_key=G4Y49K8IMI36URKX&field1=" +
+               String(gps.location.lng()) + "&field2=" +
+               String(gps.location.lat()) + "&field3=" +
+               String(time) + "&field4=normal";
+  Serial.println(str);
+  gprsSerial.println(str);
+  delay(4000);
+  ShowSerialData();
+   gprsSerial.println((char)26);
+  delay(5000);
+  gprsSerial.println();
+  ShowSerialData();
+}
+
+void closeConnection() {
+  gprsSerial.println("AT+CIPSHUT");
+  delay(100);
+  ShowSerialData();
+}
+
+void ShowSerialData() {
+  while (gprsSerial.available() != 0)
+    Serial.write(gprsSerial.read());
+  delay(5000);
+}
+
+void buttonPressed() {
+  buttonState = digitalRead(2);
+  if (buttonState == 1) {
+    delay(10);
+    buttonState = digitalRead(2);
+    if (buttonState == 0) {
+      DateTime now = rtc.now();
+      sprintf(time, "%02d:%02d:%02d %02d-%02d-%02d", now.hour(), now.minute(), now.second(), now.day(), now.month(), now.year());
+      Serial.print(F("Date/Time: "));
+      Serial.println(time);
+      delay(1000);
+
+      if (ss.available()) {
+        gps.encode(ss.read());
+        if (gps.location.isUpdated()) {
+          float longitude = gps.location.lng();
+          float latitude = gps.location.lat();
+          Serial.print("Latitude = ");
+          Serial.print(latitude, 8);
+          Serial.print(" Longitude = ");
+          Serial.println(longitude, 8);
+        }
+      }
+
+      if (gprsSerial.available()) {
+        Serial.write(gprsSerial.read());
+        executeCommands();
+        sendPanicData();
+      }
+    }
+  } else {
+    delay(100);
+  }
+}
+
+void sendPanicData() {
+  String str = "GET https://api.thingspeak.com/update?api_key=G4Y49K8IMI36URKX&field1=" +
+                String(gps.location.lng()) + "&field2=" +
+                String(gps.location.lat()) + "&field3=" +
+                String(time) + "&field4=panic";
+  Serial.println(str);
+  gprsSerial.println(str);
+  delay(4000);
+  ShowSerialData();
+  gprsSerial.println((char)26);
+  delay(5000);
+  gprsSerial.println();
+  ShowSerialData();
 }
